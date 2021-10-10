d5dd683ec179323cbbc5cd1c659286f34b746f7b
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-10-10 18:41:31 +0330

    add Setup section to the main

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+// OrbitControls allows us to move around the scene using our mouse
+import { OrbitControls } from "three/examples/jsm/controls/orbitcontrols";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// Setup:
+
+// Scene is like a container that holds all of your objects cameras and lights
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+// Make it fullscreen canvas by setting the renderer size to window size
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+// Render in renderer means draw it
+renderer.render(scene, camera);
